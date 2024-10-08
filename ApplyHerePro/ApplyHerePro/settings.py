e0e3c9fc692becfb1604337456c7d88f9755eff9
praveen <praveenreddiboina@gmail.com>
2024-10-08 18:09:27 +0530

    smtp commit

--- ApplyHerePro/ApplyHerePro/settings.py
+++ ApplyHerePro/ApplyHerePro/settings.py
@@ -193,5 +193,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '' #email
-EMAIL_HOST_PASSWORD = '' #two step varification key
+EMAIL_HOST_USER = 'praveenreddiboina1614@gmail.com'
+EMAIL_HOST_PASSWORD = 'wugfbqjtorkgrcll'
